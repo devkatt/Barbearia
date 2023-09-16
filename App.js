da6776c37c128492cdfc6c04cda8e1b012ce7751
teste <katarina.alves240@gmail.com>
2023-09-16 12:00:58 -0300

    DM

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
 import UserContextProvider from './src/contexts/UserContext';
 import MainStack from './src/stacks/MainStack';
 
