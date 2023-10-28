995cf8fc31a64023b73ea211c8375eb3368bd4ac
Katarina <89870229+devkatt@users.noreply.github.com>
2023-10-27 23:10:59 -0300

    arquivos do crud

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /**
  * @format
  */
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
-import App from './App';
-registerRootComponent(App);
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
